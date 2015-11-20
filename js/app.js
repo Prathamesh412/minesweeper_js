cc6918a94b18360f131aeef07d1219060d27d1bb
Prathamesh <prathprabhu@gmail.com>
2015-11-20 11:33:52 +0530

    Bombs added

--- js/app.js
+++ js/app.js
@@ -19,15 +19,19 @@
           //  this.rows=rows;
             //bombStack: {}
 
+        
+    ///////////////////--init funtion----
           this.init = function(){
           for(var i = 0; i<=6; i++)
               for(var j=0; j<=6; j++)
                 this.tiles.push(new tile(i,j));
           };
 
-        this.bombshell = [[2,1],[0,0],[]]
+      //  this.bombshell = [[2,1],[0,0],[]]//
+        
+        ///////////---drawboard func--------
         this.drawBoard = function(){
-            var boardDiv = document.createElement('div');
+        var boardDiv = document.createElement('div');
         boardDiv.className = 'board';
         boardDiv.setAttribute('id','board');
         var row=0;
@@ -58,32 +62,48 @@
         document.body.appendChild(boardDiv);
      };
 
-        this.generateBomb = function(numBombs) {  //push to bomsmshell..for loop to check for combination exists
-                var i;
-                var numBombs = 10;
-                for(i=0; i<numBombs; i++){   
-                var p = Math.floor(Math.random() * 6);
-               // var q = Math.floor(Math.random() * 3);
-                console.log(p);
+        //// function not called --to be removed
+//        this.generateBomb = function(numBombs) {  //push to bomsmshell..for loop to check for combination exists
+//                var i;
+//                var numBombs = 10;
+//                for(i=0; i<numBombs; i++){   
+//                var p = Math.floor(Math.random() * 6);
+//               // var q = Math.floor(Math.random() * 3);
+//                console.log(p);
+//                }
+//        };
+            
+   this.bombs = [[]];
+       // this.bombs[];
+        this.generateBomb = function() {
+            for(var i=0;i<10;i++){        // for random no generation
+                     document.getElementById('tile['+Math.floor(Math.random(i) * 49)+']').innerHTML = 'B';
+            }
+            
+            console.log(this.bombs)
+		  	while (this.bombs.length < 7)
+			{    
+                var found = false;
+				var r = Math.floor(Math.random() * 7) + 1;
+				var c = Math.floor(Math.random() * 7) + 1;
+				for(i = 0; i <this.bombs.length; i++)
+				{
+				if(r == this.bombs[i][0] && c == this.bombs[i][1]){
+                    found = true;
+                    break;
                 }
-        };
-    };
+                if(!found)
+                    this.bombs.push([r,c]);
+				}
+			}
 
-this.genBomb                         // sirs logic
-  while this.bomb.length < 7
-    Create random r
-    Create random c
-    loop to this.bomb.length
-      if(r==this.bomb[i][0] && c==this.bomb[i][1])
-        found = true;
-        break;
-    if(!found)
-        this.bomb.push([r,c])
-
-
+               
+        };//end bombs
+};        
     var myBoard = new board();    
     myBoard.init();
     myBoard.drawBoard();
     myBoard.generateBomb();
+   // myBoard.bombs();
     
 })();
